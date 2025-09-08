b886f2c4bed50f3d0783ea79a4c2abe30047bf3a
adam <awlapoint@gmail.com>
2025-09-07 21:43:04 -0400

    check point, working on optimized version

--- jump-game.py
+++ jump-game.py
@@ -5,27 +5,48 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums)
-        dp = [0] * n
+        if n == 1:
+            return 0
 
-        for i in range(1, n):
-            jumps = []
-            for j in range(0, i):
-                if j + nums[j] >= i:
-                    jumps.append(dp[j] + 1)
-            dp[i] = min(jumps)
+        jumps = 0
 
-        return dp[-1]
+        i = 0
+        while i <= n - 2:
+            # if current position gets us to the end
+            if i + nums[i] >= n - 1:
+                return jumps + 1
+
+            bucket = []
+            for j in range(i + 1, nums[i] + 1):
+                if j + 1 >= n - 1:
+                    return jumps + 2
+                bucket.append((j + nums[j], j))
+
+            if not bucket:
+                i += 1
+                jumps += 1
+                continue
+
+            best = max(bucket)
+            if i + best[1] >= n - 1:
+                return jumps + 1
+            i = best[1]
+            jumps += 1
+
+        return jumps
 
 
 solution = Solution()
 
 test1 = [2,3,1,1,4]
 test2 = [2,3,0,1,4]
+test3 = [0]
 
 result1 = 2
 result2 = 2
+result3 = 0
 
-tests = [[test1, result1], [test2, result2]]
+tests = [[test1, result1], [test2, result2], [test3, result3]]
 
 for test in tests:
     result = solution.jump(test[0])
@@ -33,3 +54,11 @@ for test in tests:
         print(f"failed {test} result {result}")
     else:
         print(f"passed {test} result {result}")
+
+#print(solution.jump([1]))
+#print(solution.jump([2,1]))
+#print(solution.jump([2,3,1]))
+#print(solution.jump([1,1,1,1]))
+#print(solution.jump([2,3,1,1,4]))
+print(solution.jump([1,2,3]))
+print(solution.jump([1,2,1,1,1]))
